eaca6bd4dae81e623933ef53715523d563686174
付一洲 <fuyizhou@svengine.com>
2020-07-09 17:51:25 +0800

    增加dispatch机制

---
+++ SVEngine/src/app/SVDispatch.cpp
@@ -0,0 +1,36 @@
+//
+// SVDispatch.cpp
+// SVEngine
+// Copyright 2017-2020
+// yizhou Fu,long Yin,longfei Lin,ziyu Xu,xiaofan Li,daming Li
+//
+
+#include "SVDispatch.h"
+#include "SVInst.h"
+#include "SVGlobalMgr.h"
+#include "SVGlobalParam.h"
+#include "../basesys/SVRPath.h"
+#include "../work/SVTdCore.h"
+#include "../work/SVThreadPool.h"
+#include "../file/SVFileMgr.h"
+#include "../basesys/SVBasicSys.h"
+#include "../basesys/SVConfig.h"
+#include "../operate/SVOpBase.h"
+#include "../operate/SVOpCreate.h"
+#include "../operate/SVOpThread.h"
+#include "../rendercore/SVRenderMgr.h"
+#include "../rendercore/SVRenderState.h"
+#include "../rendercore/SVRenderer.h"
+#include "../rendercore/SVMetal/SVRendererMetal.h"
+#include "../rendercore/SVGL/SVRendererGL.h"
+
+using namespace sv;
+
+void SVDispatch::dispatchShaderCreate(SVInstPtr _app,SVRShaderPtr _shader) {
+    
+}
+
+//投递mesh create
+void SVDispatch::dispatchMeshCreate(SVInstPtr _app,SVRMeshPtr _mesh) {
+    
+}
---
+++ SVEngine/src/app/SVDispatch.h
@@ -0,0 +1,37 @@
+//
+// SVInst.h
+// SVEngine
+// Copyright 2017-2020
+// yizhou Fu,long Yin,longfei Lin,ziyu Xu,xiaofan Li,daming Li
+//
+
+#ifndef SV_DISPATCH_H
+#define SV_DISPATCH_H
+
+#include "../base/SVIDPool.h"
+#include "../rendercore/SVRenderDeclare.h"
+#include "SVGlobalMgr.h"
+#include "SVGlobalParam.h"
+
+namespace sv {
+    
+    /*
+     分发 投递器
+     */
+
+    class SVDispatch : public SVObject {
+    public:
+        //投递shader create
+        static void dispatchShaderCreate(SVInstPtr _app,SVRShaderPtr _shader);
+        
+        //投递mesh create
+        static void dispatchMeshCreate(SVInstPtr _app,SVRMeshPtr _mesh);
+        //
+       
+    };
+    
+}//!namespace sv
+
+
+
+#endif //SV_DISPATCH_H
