fa89b9128614296922a2b2604146c713cdf08c58
付一洲 <fuyizhou@svengine.com>
2020-08-28 15:34:24 +0800

    田间滤镜库类

---
+++ SVEngine/src/basesys/filter/SVFilterLib.cpp
@@ -0,0 +1,16 @@
+//
+// SVFilterLib.cpp
+// SVEngine
+// Copyright 2017-2020
+// yizhou Fu,long Yin,longfei Lin,ziyu Xu,xiaofan Li,daming Li
+//
+
+#include "SVFilterLib.h"
+#include "../app/SVInst.h"
+#include "../file/SVFileMgr.h"
+
+using namespace sv;
+
+SVFilterBasePtr SVFilterLib::createFilter(s32 _type) {
+    return nullptr;
+}
---
+++ SVEngine/src/basesys/filter/SVFilterLib.h
@@ -0,0 +1,30 @@
+//
+// SVFilterLib.h
+// SVEngine
+// Copyright 2017-2020
+// yizhou Fu,long Yin,longfei Lin,ziyu Xu,xiaofan Li,daming Li
+//
+
+#ifndef SV_FILTERLIB_H
+#define SV_FILTERLIB_H
+
+#include "../base/SVGBase.h"
+
+namespace sv {
+
+    
+    /*引擎提供的默认滤镜库*/
+
+    //特定滤镜，不允许更换材质
+    //公共滤镜，可以替换材质
+    
+    class SVFilterLib : public SVGBaseEx {
+    public:
+        static SVFilterBasePtr createFilter(s32 _type);
+        
+    };
+
+}//!namespace sv
+
+
+#endif //SV_FILTERLIB_H
